1fafa1489d3b93c7c6908cc6a56ff5bc6e336a1a
Nichole Boothman <nbooth731@gmail.com>
2021-12-02 17:00:00 -0500

    Complete

---
+++ Cipher/cipher.js
@@ -0,0 +1,26 @@
+
+
+const cipher = (phrase) => {
+let decipher = 4
+let solution = ""
+for (let i = 0; i < phrase.length; i++){
+    let cipheredPhrase = phrase[i].charCodeAt() + decipher
+    solution += String.fromCharCode(cipheredPhrase)
+}
+    return solution
+}
+
+console.log(cipher("I love cryptography"))
+
+const decipher = (phrase) => {
+    let key = 4
+    let solution = ""
+for (let i = 0; i < phrase.length; i++){
+    let cipheredPhrase = phrase[i].charCodeAt() - key
+    solution += String.fromCharCode(cipheredPhrase)
+}
+    return solution
+}
+
+console.log(decipher(cipher("I love cryptography")))
+
